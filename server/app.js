15ddda195bfb8f5d51a0b14a3ca139a5e63014db
Lily Lu <lilyzhaoyilu@gmail.com>
2023-11-13 20:54:56 -0800

    update config

--- server/app.js
+++ server/app.js
@@ -19,7 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/', mainRouter);
 app.use('/q', queryRouter);
@@ -40,6 +41,11 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Serve React app for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 module.exports = app;
